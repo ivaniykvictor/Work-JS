5200543991f4f82f3c5b78e1c9949bc510a4c16a
Victor <ivaniykvictor@gmail.com>
2020-03-30 11:34:08 +0300

    favoriteFilm

---
+++ Lecture 15/Task1/js/main.js
@@ -0,0 +1,23 @@
+console.log("Task 1");
+
+const film = {
+    name: "Ocean's Eleven",
+    yearOfCreation: 2001,
+    genre: "thriller",
+    cashInDollars: 450717150,
+    moviePaidOff: true,
+    directedBy: "Steven Soderbergh",
+    rating: 7.8,
+    actors: ["George Clooney", "Brad Pitt", "Don Cheadle", "Bernie Mac", "Julia Roberts"],
+};
+console.log(film);
+console.log(Object.keys(film));
+console.log(film.name);
+console.log(film.yearOfCreation);
+console.log(film.genre);
+console.log(film.cashInDollars);
+console.log(film.moviePaidOff);
+console.log(film.directedBy);
+console.log(film.rating);
+console.log(film.actors);
+
