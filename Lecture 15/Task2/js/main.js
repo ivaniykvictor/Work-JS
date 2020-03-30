e91c7180cbd0bdd4f3c7d4ff57db4b4b899ba407
Victor <ivaniykvictor@gmail.com>
2020-03-30 11:44:44 +0300

    tvShow

---
+++ Lecture 15/Task2/js/main.js
@@ -0,0 +1,14 @@
+console.log("Task2");
+const tvShow = [
+    "X-factor", 
+    "Світське життя", 
+    "Суперінтуїція", 
+    "Голос країни", 
+    "Хто зверху"
+];
+tvShow.push(prompt("Please add a new show"));
+const tvShowСomma = tvShow.join();
+const tvShowSpace = tvShow.join("| ");
+
+console.log(tvShowСomma);
+console.log(tvShowSpace);
