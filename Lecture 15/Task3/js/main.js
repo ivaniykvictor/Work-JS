392113c8698093c30f716fa7c2fdc3d27cf22c84
Victor <ivaniykvictor@gmail.com>
2020-03-30 11:52:17 +0300

    circle

---
+++ Lecture 15/Task3/js/main.js
@@ -0,0 +1,8 @@
+console.log("Task3");
+let circleRadius =  Math.floor(Math.random(10)*100);
+let circuit = Math.floor(2 * Math.PI * circleRadius);
+let area = Math.floor(Math.PI * (Math.pow(circleRadius, 2)));
+
+console.log("Радіус кола =",circleRadius,"cm:");
+console.log("Довжина кола =",circuit,"cm:");
+console.log("Площа кола =",area,"cm:");
