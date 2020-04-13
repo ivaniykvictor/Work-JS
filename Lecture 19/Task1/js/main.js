035ead73464c85aa01e85dfd4093455c85b40985
Victor <ivaniykvictor@gmail.com>
2020-04-13 10:16:37 +0300

    function

---
+++ Lecture 19/Task1/js/main.js
@@ -0,0 +1,18 @@
+let circuit = (radius) => 2 * Math.PI * radius;
+console.log(circuit(6));
+
+let calculateArea = (width, height) => {
+    return width * height;
+};
+console.log(calculateArea(5, 5));
+
+let areaSquare = function (widht) {
+    return Math.pow(widht, 2);
+};
+console.log(areaSquare(7));
+
+function conversionValute(suma, kurs) {
+    return suma * kurs;
+}
+console.log(conversionValute(300, 27))
+
