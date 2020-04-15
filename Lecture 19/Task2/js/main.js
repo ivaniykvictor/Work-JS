1f07a5b77b586cc4f0dbed3f2e2cef684597c5fc
Victor <ivaniykvictor@gmail.com>
2020-04-15 15:13:05 +0300

    currencyСalculator

--- Lecture 19/Task2/js/main.js
+++ Lecture 19/Task2/js/main.js
@@ -1,21 +1,38 @@
-const currency = document.getElementById('currency');
-currency.addEventListener("click", function () {
-    const currencyElement = document.getElementById('currency');
-    const currency = currencyElement.value;
-    console.log(currency);
-    const currencyValue = document.getElementById("currencyValue");
-    currencyValue.innerHTML = currency + " гривень за одиницю";
-}); 
+const usd = 27.4;
+const eur = 30;
+const rub = 3.45;
+const bgn = 15;
+const ron = 8;
+
 const calculateButton = document.getElementById('calculateButton');
 calculateButton.addEventListener("click", function () {
+   
+    const currencyElement = document.getElementById('currency').value;
+
+    let currencyCours;
+    switch (currencyElement) {
+        case 'USD':
+            currencyCours = usd
+            break
+        case 'EUR':
+            currencyCours = eur
+            break;
+        case 'RUB':
+            currencyCours = rub
+            break;
+        case 'BGN':
+            currencyCours = bgn
+            break
+        case 'RON':
+            currencyCours = ron
+            break
+    };
+    const currencyValue = document.getElementById("currencyValue");
+    currencyValue.innerHTML = currencyCours + " гривень за одиницю";
     const amountElement = document.getElementById('amount');
     const amount = (amountElement.value);
-    const currencyElement = document.getElementById('currency');
-    const currency = currencyElement.value;
-    const suma = currency * amount;
+    let suma = currencyCours * amount;
     const resultElement = document.getElementById("result");
     resultElement.innerHTML = "за " + amount + " одиниць валюти в гривневому еквіваленті ви отримаєте " + suma + " грн.";
 });
-
-
 
