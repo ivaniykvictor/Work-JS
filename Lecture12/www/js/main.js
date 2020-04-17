9f105345b8d2c7a16af05a43e9d3bfcbf434565a
Victor <ivaniykvictor@gmail.com>
2020-04-17 14:22:33 +0300

    myFirstLeding

---
+++ Lecture12/www/js/main.js
@@ -0,0 +1,11 @@
+const headerBlock = document.getElementById('headerBlock');
+
+headerBlock.addEventListener ('scroll', function (){
+    const headerBlockElement = document.getElementById('headerBlock')
+    headerBlockElement.className = 'header__block-id'
+});
+    
+
+
+
+    
