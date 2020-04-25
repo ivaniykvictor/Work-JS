e40ab999a75e8f1391c65de2f1b2e7d7e41570f7
Vadym Moiseyenko <vadym.moiseyenko@agiliway.com>
2020-04-26 02:22:00 +0300

    Add comments

--- Lecture12/www/js/main.js
+++ Lecture12/www/js/main.js
@@ -1,23 +1,31 @@
-window.onscroll = function() {scrollFunction()};
+(function(){
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("headerBlock").style.height = "75px";
-  } else {
-    document.getElementById("headerBlock").style.height = "117px";
-  }
-};
+  window.onscroll = function() {
+    scrollFunction()
+  };
+  
+  function scrollFunction() {
+    const headerBlockElement = document.getElementById("headerBlock");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      // try to avoid inline styling as much as possible, rewrite it to add remove classes
+      headerBlockElement.style.height = "75px";
+    } else {
+      headerBlockElement.style.height = "117px";
+    }
+  };
+  
+  const menuActiveElement = document.getElementById('menuActive');
+  
+  const adaptiveMenu = document.getElementById('adaptiveMenu');
+  adaptiveMenu.addEventListener('click', function() {
+    menuActiveElement.classList.add("active");
+  });
+  
+  const menuClose = document.getElementById('menuClose');
+  menuClose.addEventListener('click', function() {
+    menuActiveElement.classList.remove("active");
+  });
 
-const adaptiveMenu = document.getElementById('adaptiveMenu');
-
-adaptiveMenu.addEventListener('click', function() {
-  document.getElementById('menuActive').classList.add("active");
-});
-
-const menuClose = document.getElementById('menuClose');
-
-menuClose.addEventListener('click', function() {
-  document.getElementById('menuActive').classList.remove("active");
-});
-
+})()
+  
 
