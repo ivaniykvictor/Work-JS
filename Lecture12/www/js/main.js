f85e3f188829bc3e779ebb51728f7ba9b920fdd6
Victor <ivaniykvictor@gmail.com>
2020-04-26 14:00:56 +0300

    myFirstLeding

--- Lecture12/www/js/main.js
+++ Lecture12/www/js/main.js
@@ -1,23 +1,30 @@
-window.onscroll = function() {scrollFunction()};
+(function(){
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("headerBlock").style.height = "75px";
-  } else {
-    document.getElementById("headerBlock").style.height = "117px";
-  }
-};
+  window.onscroll = function() {
+    scrollFunction()
+  };
+  
+  function scrollFunction() {
+    const headerBlockElement = document.getElementById("headerBlock");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      headerBlockElement.classList.add("header__block-small");
+    } else {
+      headerBlockElement.classList.remove("header__block-small");
+    }
+  };
+  
+  const menuActiveElement = document.getElementById('menuActive');
+  
+  const adaptiveMenu = document.getElementById('adaptiveMenu');
+  adaptiveMenu.addEventListener('click', function() {
+    menuActiveElement.classList.add("active");
+  });
+  
+  const menuClose = document.getElementById('menuClose');
+  menuClose.addEventListener('click', function() {
+    menuActiveElement.classList.remove("active");
+  });
 
-const adaptiveMenu = document.getElementById('adaptiveMenu');
-
-adaptiveMenu.addEventListener('click', function() {
-  document.getElementById('menuActive').classList.add("active");
-});
-
-const menuClose = document.getElementById('menuClose');
-
-menuClose.addEventListener('click', function() {
-  document.getElementById('menuActive').classList.remove("active");
-});
-
+})()
+  
 
